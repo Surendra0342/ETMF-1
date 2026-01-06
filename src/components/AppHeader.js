6a97104b4280bf51ef1e15e2914242240b0efda5
Surendra0342 <surendrachinn143@gmail.com>
2026-01-06 18:04:36 +0530

    fixed search bar issue in table component

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -53,7 +53,7 @@ const AppHeader = () => {
         >
           <CIcon icon={cilMenu} size="lg" />
         </CHeaderToggler>
-        <CHeaderNav className="d-none d-md-flex">
+        {/* <CHeaderNav className="d-none d-md-flex">
           <CNavItem>
             <CNavLink to="/dashboard" as={NavLink}>
               Dashboard
@@ -65,7 +65,7 @@ const AppHeader = () => {
           <CNavItem>
             <CNavLink href="#">Settings</CNavLink>
           </CNavItem>
-        </CHeaderNav>
+        </CHeaderNav> */}
         <CHeaderNav className="ms-auto">
           <CNavItem>
             <CNavLink href="#">
